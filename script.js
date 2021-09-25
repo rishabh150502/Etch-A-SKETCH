0d983388126d8097bf16252ab376da44a231cc48
Rishabh <itsrk1505@gmail.com>
2021-09-25 12:27:09 +0530

    edit  grid size

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 console.log('hello');
 let drawState=false;
 const canvas=document.querySelector('.canvas');
-for(let i=0;i<(16*16);i++){
+function createPixels(n){
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.firstChild);
+    }
+for(let i=0;i<(n*n);i++){
     const pixels=document.createElement('div');
     pixels.classList.add('pixels');
-    pixels.style.cssText=`height: ${100/16}%; width: ${100/16}%;`;
+    pixels.style.cssText=`height: ${100/n}%; width: ${100/n}%;`;
     canvas.append(pixels);
     console.log(i);
 }
+}
+createPixels(16);
 window.addEventListener('mousedown',function(){
     drawState=true
     console.log(true);
@@ -24,5 +30,19 @@ for(i of pixels){
         if(drawState){
         this.classList.add('color');
         }
+    });
+
+    const btn=document.querySelector('.clear');
+    btn.addEventListener('click',function(){
+        for(i of pixels){
+            i.classList.remove('color');
+        }
+    });
+
+    const select=document.querySelector('#grid-size');
+    select.addEventListener('change',function(){
+        console.log(select.value);
+        let size=select.value;
+        createPixels(size);
     });
 }
