e76d770ea3e4466b19b3b5be87983ad34f5ccd15
rishabh150502 <itsrk1505@gmail.com>
2021-09-26 15:53:08 +0530

    javascript code edit

--- script.js
+++ script.js
@@ -1,66 +1,79 @@
 console.log('hello');
-let drawState = false;
+
 const canvas = document.querySelector('.canvas');
-let pixels = document.querySelectorAll('.pixels');
-function createPixels(n) {
-    while (canvas.firstChild) {
-        canvas.removeChild(canvas.firstChild);
-    }
-    for (let i = 0; i < (n * n); i++) {
+const clear = document.querySelector('.clear');
+const erase = document.querySelector('.erase');
+const select = document.querySelector('select');
+const colorPanel = document.querySelector('input');
+let grid = document.querySelectorAll('.pixels');
+let drawState = false;
+let eraseState = false;
+let grid_value = 16;
+let currentColor=colorPanel.value;
+
+function createGrid(canvas, value) {
+    canvas.innerHTML = "";
+    for (let i = 0; i < (value * value); i++) {
         const pixels = document.createElement('div');
         pixels.classList.add('pixels');
-        pixels.style.cssText = `height: ${100 / n}%; width: ${100 / n}%;`;
+        pixels.classList.add(`${i}`);
+        pixels.style.cssText = `height: ${100 / value}%; width: ${100 / value}%;`;
         canvas.append(pixels);
-        console.log(i);
     }
-    pixels = document.querySelectorAll('.pixels');
-    for (i of pixels) {
+    grid = document.querySelectorAll('.pixels');
+    for (i of grid) {
         i.addEventListener('mouseenter', function () {
-            console.log(this);
-            if (drawState) {
-                this.classList.add('color');
-            }
+            draw(drawState, this);
         });
     }
 }
 
-createPixels(16);
-
-window.addEventListener('mousedown', function (e) {
-    if(e.button===0){
-    drawState = true;
-    e.stopPropagation();
+function draw(drawState, x) {
+    if (eraseState === true && drawState === true) {
+        x.style["background-color"] = "white";
     }
-    console.log(e.button);
+    else if (drawState === true && eraseState === false) {
+        x.style["background-color"] = `${currentColor}`;
+    }
+
+}
+//Draw State check
+window.addEventListener('mousedown', function (e) {
+    if (e.button === 0) {
+        drawState = true;
+    }
 });
 window.addEventListener('mouseup', function (e) {
-    if(e.button===0){
-    drawState = false;
-    e.stopPropagation();
-    }
-    console.log(e.button);
-});
-
-const btn = document.querySelector('.clear');
-btn.addEventListener('click', function () {
-    for (i of pixels) {
-        i.classList.remove('color');
+    if (e.button === 0) {
+        drawState = false;
     }
 });
 
-const select = document.querySelector('#grid-size');
-select.addEventListener('change', function () {
-    console.log(select.value);
-    let size = select.value;
-    createPixels(size);
+//clear
+clear.addEventListener('click', () => {
+    createGrid(canvas, grid_value);
 });
 
-const inputs=document.querySelector('input');
-inputs.addEventListener('change',function(){
-    console.log(this.value);
-    document.documentElement.style.setProperty(`--base`,this.value);
+erase.addEventListener('click', () => {
+    if (eraseState === false) {
+        erase.textContent = 'draw';
+        eraseState = true;
+    }
+    else {
+        erase.textContent = 'erase';
+        eraseState = false;
+    }
 });
-inputs.addEventListener('input',function(){
-    console.log(this.value);
-    document.documentElement.style.setProperty(`--base`,this.value);
+
+select.addEventListener('change', function() {
+    grid_value= this.value;
+    createGrid(canvas, this.value);
 });
+
+colorPanel.addEventListener('input', function(){
+    currentColor=this.value;
+});
+
+window.onload = (e) => {
+    createGrid(canvas, grid_value);
+};
